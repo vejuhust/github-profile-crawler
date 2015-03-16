33fd11ceb518769a9b5cb86fff26fb7cd67e0507
Wei Ye <vejuhust@gmail.com>
2015-03-17 01:28:42 +0800

    Add BaseLogger.py with basic implementation for both console and file

---
+++ BaseLogger.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Basic logger to support other workers for github profile crawler"""
+
+import sys
+import logging
+from contextlib import closing
+from config import config_log_file
+
+
+class BaseLogger():
+    def __init__(self, level=logging.INFO):
+        self._log_formatter = logging.Formatter("%(asctime)s [%(module)s][%(process)d][%(levelname)s] %(message)s")
+        self._root_logger = logging.getLogger()
+        self._root_logger.setLevel(level)
+        self._root_logger.addHandler(self._file_handler())
+        self._root_logger.addHandler(self._console_handler())
+
+
+    def _file_handler(self):
+        file_handler = logging.FileHandler(config_log_file)
+        file_handler.setFormatter(self._log_formatter)
+        return file_handler
+
+
+    def _console_handler(self):
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(self._log_formatter)
+        return console_handler
+
+
+    def critical(self, message):
+        self._root_logger.critical(message)
+
+
+    def error(self, message):
+        self._root_logger.error(message)
+
+
+    def exception(self, message):
+        self._root_logger.exception(message)
+
+
+    def warning(self, message):
+        self._root_logger.warning(message)
+
+
+    def info(self, message):
+        self._root_logger.info(message)
+
+
+    def debug(self, message):
+        self._root_logger.debug(message)
+
