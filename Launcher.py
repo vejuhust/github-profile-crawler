be08a1bcac35c6c4d7ddadbf975418d80b5f2ba7
Wei Ye <vejuhust@gmail.com>
2015-03-17 02:41:41 +0800

    Turn Launcher.py into a real class Launcher(BaseLogger)

--- Launcher.py
+++ Launcher.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Web crawler for github profile crawler"""
 
-from config import *
-from contextlib import closing
 from Assigner import Assigner
+from BaseLogger import BaseLogger
 from Crawler import Crawler
 from DatabaseAccessor import DatabaseAccessor
+from config import *
+from contextlib import closing
+from contextlib import closing
 from time import sleep
 
 
@@ -37,36 +39,51 @@ urls = [
 ]
 
 
-def add_urls_to_queue_crawl():
-    with closing(DatabaseAccessor()) as dal:
-        for url in urls:
-            print("add {} - {}".format(url, dal.queue_crawl_create(url)))
+class Launcher(BaseLogger):
+    def __init__(self):
+        BaseLogger.__init__(self)
 
 
-def clear_queue_crawl_and_page():
-    with closing(DatabaseAccessor()) as dal:
-        print("clear crawl - {}".format(dal.queue_crawl_clear()))
-        print("clear page - {}".format(dal.queue_page_clear()))
+    def process(self, urls):
+        self.clear_queue_crawl_and_page()
+        self.add_urls_to_queue_crawl(urls)
+        self.run_crawler(len(urls))
+        self.run_assigner(len(urls))
 
 
-def run_crawler(times=5):
-    with closing(Crawler()) as crawler:
-        for _ in range(times):
-            print("crawl - {}".format(crawler.process()))
-            sleep(config_crawl_sleep)
+    def add_urls_to_queue_crawl(self, urls):
+        with closing(DatabaseAccessor()) as dal:
+            for url in urls:
+                self._log_info("add {} - {}".format(url, dal.queue_crawl_create(url)))
 
 
-def run_assigner(times=5):
-    with closing(Assigner()) as assigner:
-        for _ in range(times):
-            print("assign - {}".format(assigner.process()))
+    def clear_queue_crawl_and_page(self):
+        with closing(DatabaseAccessor()) as dal:
+            self._log_info("clear crawl - {}".format(dal.queue_crawl_clear()))
+            self._log_info("clear page - {}".format(dal.queue_page_clear()))
+
+
+    def run_crawler(self, times=5):
+        with closing(Crawler()) as crawler:
+            for _ in range(times):
+                self._log_info("crawl - {}".format(crawler.process()))
+                sleep(config_crawl_sleep)
+
+
+    def run_assigner(self, times=5):
+        with closing(Assigner()) as assigner:
+            for _ in range(times):
+                self._log_info("assign - {}".format(assigner.process()))
+
+
+    def close(self):
+        self._log_info("bye bye!")
+        self._close_logger()
 
 
 def main():
-    clear_queue_crawl_and_page()
-    add_urls_to_queue_crawl()
-    run_crawler(len(urls))
-    run_assigner(len(urls))
+    with closing(Launcher()) as launcher:
+        launcher.process(urls)
 
 
 if __name__ == '__main__':
