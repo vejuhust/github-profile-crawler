06c9742d04e228c2dfee2bf42c2c31e37247e1f8
Wei Ye <vejuhust@gmail.com>
2015-03-18 02:28:59 +0800

    Clean up functionality in ParserFollow.py

--- ParserFollow.py
+++ ParserFollow.py
@@ -18,19 +18,17 @@ class ParserFollow(BaseLogger):
     def process(self):
         status = False
         job = self._db_conn.queue_page_take_follow()
-        self._db_conn._job_update(config_queue_page, None, "follow", job['url'])
+        # self._db_conn._job_update(config_queue_page, None, "follow", job['url'])
         if job != None:
             url = job['url']
             text = job.get('text', "")
             links = self._parse_user_links(url, text)
-            pprint(links)
-            pprint(len(links))
             self._log_info(url)
-            # if links:
-            #     for link in links:
-            #         self._db_conn.queue_crawl_create(link)
-            #     self._db_conn.queue_page_done_follow(url)
-            #     status = True
+            if links:
+                for link in links:
+                    self._db_conn.queue_crawl_create(link)
+                self._db_conn.queue_page_done_follow(url)
+                status = True
         return status
 
 
@@ -62,43 +60,13 @@ class ParserFollow(BaseLogger):
         return purged
 
 
-    def _parse_tag_text_by_itemprop(self, soup, item_name):
-        tags = soup.find_all(itemprop=item_name)
-        if len(tags) > 0:
-            return tags[0].text
-
-
-    def _parse_tag_string_by_class(self, soup, class_name):
-        tags = soup.find_all(class_=class_name)
-        if len(tags) > 0:
-            return tags[0].string
-
-
-    def _parse_tag_datetime_by_class(self, soup, class_name):
-        tags = soup.find_all(class_=class_name)
-        if len(tags) > 0:
-            return tags[0].get('datetime')
-
-
-    def _parse_tag_count_and_link(self, soup, text):
-        tags = soup.find_all(class_="pagination")
-        count = None
-        link = None
-        for tag in tags:
-            if "Next" in tag.find(rel="nofollow").text:
-                count = tag.find(class_="vcard-stat-count").text
-                link = tag.get('href')
-                break
-        return count, link
-
-
     def close(self):
         self._close_logger()
 
 
 def main():
     with closing(ParserFollow()) as parser:
-        for _ in range(1):
+        for _ in range(30):
             print("No. {} --".format(_ + 1))
             pprint(parser.process())
 
