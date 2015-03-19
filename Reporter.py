42c6cf5912127a018342ff39e27797b1d13cc1ff
Wei Ye <vejuhust@gmail.com>
2015-03-19 20:48:34 +0800

    Update Reporter.py with essential status logging and save status into json file

--- Reporter.py
+++ Reporter.py
@@ -6,9 +6,11 @@ from DatabaseAccessor import DatabaseAccessor
 from bs4 import BeautifulSoup
 from config import config_report_interval
 from contextlib import closing
-from json import dumps
+from datetime import datetime
+from json import loads, dumps
+from os.path import isfile
 from platform import node
-from time import sleep
+from time import sleep, strftime
 
 
 class Reporter(BaseLogger):
@@ -19,19 +21,55 @@ class Reporter(BaseLogger):
 
 
     def process(self):
+        self.update_data()
+        return
+
+
+    def update_data(self):
+        data = self._load_data()
+        self._log_info("load existing data, count: %d", len(data))
         status = {
-            "crawl all": self._db_conn.queue_crawl_count(),
-            "crawl new": self._db_conn.queue_crawl_count("new"),
-            "crawl fail": self._db_conn.queue_crawl_count("fail"),
-            "page all": self._db_conn.queue_page_count(),
-            "page new": self._db_conn.queue_page_count("new"),
-            "page profile": self._db_conn.queue_page_count("profile"),
-            "page follow": self._db_conn.queue_page_count("follow"),
-            "page unknown": self._db_conn.queue_page_count("unknown"),
+            "crawl_all": self._db_conn.queue_crawl_count(),
+            "crawl_new": self._db_conn.queue_crawl_count("new"),
+            "crawl_fail": self._db_conn.queue_crawl_count("fail"),
+            "page_all": self._db_conn.queue_page_count(),
+            "page_new": self._db_conn.queue_page_count("new"),
+            "page_profile": self._db_conn.queue_page_count("profile"),
+            "page_follow": self._db_conn.queue_page_count("follow"),
+            "page_unknown": self._db_conn.queue_page_count("unknown"),
             "profile": self._db_conn.profile_count(),
-            "profile w/ email": self._db_conn.profile_count("email"),
+            "profile_email": self._db_conn.profile_count("email"),
+            "date": strftime("%Y-%m-%d %H:%M:%S"),
         }
+        data.append(status)
+        self._save_data(data)
+        self._log_info("save existing data, count: %d", len(data))
         self._log_info(dumps(status, sort_keys=True, indent=4))
+
+
+    def _load_data(self, filename="status.json"):
+        data = {}
+        if isfile(filename):
+            try:
+                data_file = open(filename, 'r')
+                content = data_file.read()
+                data_file.close()
+                data = loads(content)
+            except Exception as e:
+                self._log_exception("fail to load json file: %s", filename)
+        else:
+            self._log_warning("fail to find json file: %s", filename)
+        return data
+
+
+    def _save_data(self, data, filename="status.json"):
+        output_file = open(filename, 'w')
+        output_file.write(dumps(data, sort_keys = True, indent = 4))
+        output_file.close()
+
+
+    def _extract_list(self, data, field):
+        return [ item[field] for item in data ]
 
 
     def close(self):
